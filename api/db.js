c31f38f1a190c339c6745831847e89c9337588f2
Jenny Fei <yuyans.are@gmail.com>
2024-02-08 22:07:11 +0100

    add env

--- api/db.js
+++ api/db.js
@@ -1,8 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString:
-    "postgres://default:Fosm0vel9IdM@ep-rapid-surf-a2p067z5-pooler.eu-central-1.postgres.vercel-storage.com:5432/verceldb?sslmode=require",
+  connectionString: process.env.POSTGRES_DATABASE,
 });
 
 module.exports = pool;

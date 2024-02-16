04f564617c9e3c028c080d478a9a48a3deb3e553
Jenny Fei <yuyans.are@gmail.com>
2024-02-17 00:40:20 +0100

    add graphql to get user data

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
 const citiesRoutes = require("./routes/cities");
 const itineraryRoutes = require("./routes/itinerary");
 const destinationRoutes = require("./routes/destination");
 const tripRoutes = require("./routes/trip");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schema");
+const app = express();
 
 // Middleware to parse JSON request body
 app.use(express.json());
@@ -23,3 +25,17 @@ app.use("/api/bulk_destinations", destinationRoutes);
 app.use("/api/trip", tripRoutes);
 
 app.listen(5000);
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start(); // <-- Start the Apollo Server instance
+
+  const app = express();
+  server.applyMiddleware({ app });
+
+  app.listen(4000);
+}
+
+startApolloServer().catch((err) => {
+  console.error("Error starting Apollo Server:", err);
+});
---
+++ api/schema.js
@@ -0,0 +1,56 @@
+const { gql } = require("apollo-server-express");
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_DATABASE,
+});
+
+const typeDefs = gql`
+  type User {
+    id: Int
+    firstName: String
+    lastName: String
+    email: String
+    items: [Trip]
+    createdAt: String
+    updatedAt: String
+  }
+
+  type Trip {
+    id: Int
+  }
+
+  type Query {
+    getUserById(id: Int!): User
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getUserById: async (_, { id }) => {
+      try {
+        // Query the database to fetch user by ID
+        const client = await pool.connect();
+        const queryText = `SELECT * FROM "User" WHERE id = $1`;
+        const result = await client.query(queryText, [id]);
+        client.release();
+
+        // Check if user with the provided ID exists
+        if (result.rows.length === 0) {
+          throw new Error("User not found");
+        }
+
+        // Return the user information
+        return result.rows[0];
+      } catch (error) {
+        console.error("Error fetching user by ID:", error);
+        throw error;
+      }
+    },
+  },
+};
+
+module.exports = {
+  typeDefs,
+  resolvers,
+};

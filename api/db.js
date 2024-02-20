12e7b77f426a1948c476ca4a927f237639bc832e
Jenny Fei <yuyans.are@gmail.com>
2024-02-20 23:13:15 +0100

    prisma generate

--- api/db.js
+++ api/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_DATABASE,
+  connectionString: process.env.POSTGRES_DATABASE_CONNECT,
 });
 
 module.exports = pool;
